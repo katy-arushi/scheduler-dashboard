381e4b5b2d5f27c9093d56969b83121ad175e0b7
Arushi Katyal <katy.arushi@gmail.com>
2021-12-16 01:29:24 -0500

    map over data to create panel components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 const data = [
   {
@@ -27,14 +28,24 @@ const data = [
 
 class Dashboard extends Component {
   state = { loading: false }
+  
   render() {
     const dashboardClasses = classnames("dashboard");
-
+    
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = data.map(panel => {
+      return <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    })
+    
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
